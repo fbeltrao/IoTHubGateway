db3fd8260cdfc0c1552df49235c7bc1d1a33ca73
fbeltrao <fbeltrao@windowslive.com>
2018-03-15 08:53:08 +0100

    Troubleshooting travis

--- src/SampleClients/CSharpClient/Program.cs
+++ src/SampleClients/CSharpClient/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Azure.Devices.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
